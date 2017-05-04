f6d65238a55d51275ef4229a00a865d5333f9894
shokry suleiman <shokrysuleiman032@gmail.com>
2017-05-04 02:35:10 +0200

    git commit -m

--- src/app/recipe-item/recipe-item.component.ts
+++ src/app/recipe-item/recipe-item.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, Input} from '@angular/core';
+import { Recipe } from  '../recipe';
 @Component({
   selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrls: ['./recipe-item.component.css']
+  templateUrl: './recipe-item.component.html'
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Input() recipe: Recipe;
+  recipeId: number;
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Array<Recipe>=[];
+  recipe:Recipe = new Recipe('men','menStyle','C:\Users\Shokry Suleiman\Desktop\recipe-book\src\assets\images');
   constructor() { }
 
   ngOnInit() {
