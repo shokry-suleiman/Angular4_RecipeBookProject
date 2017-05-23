8cc88786176438000d9a78cd1933d47940a2269e
shokry suleiman <shokrysuleiman032@gmail.com>
2017-05-23 13:59:46 +0200

    Open Recipes By ActivatedRoute Parameter

--- src/app/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,27 @@
-import {Component, OnInit, Input, Output} from '@angular/core';
+import {Component, OnInit } from '@angular/core';
 import {Recipe} from "../recipe";
 import {ShoppingListService} from "../services/shopping-list.service";
+import {ActivatedRoute, Params} from "@angular/router";
+import {RecipeService} from "../services/recipe.service";
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html'
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() selectedRecipe : Recipe ;
-  constructor(private sls: ShoppingListService) { }
+   selectedRecipe : Recipe ;
+   id: number;
+
+  constructor(private sls: ShoppingListService,
+              private recipeService: RecipeService,
+              private actRoute: ActivatedRoute) { }
+
   ngOnInit() {
+
+    this.actRoute.params.subscribe(( params: Params )=> {
+          this.id = +params['id'];
+          this.selectedRecipe = this.recipeService.getRecipes(this.id);
+    });
   }
 
   onAddToShoppingList(){
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -11,7 +11,11 @@ export class RecipeService {
       new Ingredient('French Fries',2),
       new Ingredient('Pork Meat', 1)
     ]),
-    new Recipe('Big Fat Burger','What else you need to say?','https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',[])
+    new Recipe('Big Fat Burger','What else you need to say?','https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',[
+
+      new Ingredient('Buns',2),
+      new Ingredient('Meat', 1)
+    ])
 
   ];
 
@@ -21,6 +25,9 @@ getRecipe(){
     return this.recipes;
 }
 
+getRecipes( index: number ) {
+  return this.recipes[index];
+}
 }
 
 export const RECIPE_SERVICE: Array<any> = [
