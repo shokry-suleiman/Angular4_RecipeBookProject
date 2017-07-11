b87bf753c6f34d813aff9d77ae55087f968bed55
shokry suleiman <shokrysuleiman032@gmail.com>
2017-07-11 16:52:12 +0200

    Syncing HTML with the form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -37,8 +37,10 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
+
   ],
   providers: [ RECIPE_SERVICE,
   SHOPPING_LIST],
