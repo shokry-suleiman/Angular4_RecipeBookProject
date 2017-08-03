4c185d7c58443724828f5efe508cfbdb796d8954
shokry suleiman <shokrysuleiman032@gmail.com>
2017-08-03 14:57:47 +0200

    Creating ShoppingList Feature Module With It's Routing Module

---
+++ src/app/shopping-list/shopping-list-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes, RouterModule} from "@angular/router";
+import {ShoppingListComponent} from "./shopping-list.component";
+
+const shoppingListRoutes: Routes = [
+
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(shoppingListRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class ShoppingListRoutingModule { }
