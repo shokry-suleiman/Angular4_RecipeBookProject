f4236d5a65ddc3efe7d1c38cc98239e71a03aec5
shokry suleiman <shokrysuleiman032@gmail.com>
2017-08-09 12:02:20 +0200

    Adding Route Protection To Lazy Loading Using The Interface canLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {AuthGuardService} from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
-  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'}
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+  //{ path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule', canLoad: [AuthGuardService]},
+  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'},
+
 ];
 
 
